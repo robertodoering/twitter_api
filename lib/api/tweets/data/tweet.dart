b884a35d801e36281ef352b0753455d0fbb930bc
Roberto Doering <rbydoering@gmail.com>
2020-02-08 19:37:18 +0100

    Update tweet model

--- lib/api/tweets/data/tweet.dart
+++ lib/api/tweets/data/tweet.dart
@@ -1,49 +1,161 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:twitter_api/api/users/data/user.dart';
 
 part 'tweet.g.dart';
 
-@JsonSerializable()
+@JsonSerializable(
+  explicitToJson: true,
+  fieldRename: FieldRename.snake,
+)
 class Tweet {
   Tweet();
 
   factory Tweet.fromJson(Map<String, dynamic> json) => _$TweetFromJson(json);
 
-  // User user;
-  // Entities entities;
-  // @JsonKey(name: extended_entities')
-  // Entities extendedEntities;
-  bool truncated;
-  // @JsonKey(name: 'created_at', fromJson: convertFromTwitterDateString)
+  /// UTC time when this Tweet was created.
+  // todo
+  // @JsonKey(fromJson: convertFromTwitterDateString)
   // DateTime createdAt;
-  bool favorited;
-  @JsonKey(name: 'id_str')
+
+  /// The string representation of the unique identifier for this Tweet.
   String idStr;
-  @JsonKey(name: 'in_reply_to_user_id_str')
-  String inReplyToUserIdStr;
-  @JsonKey(name: 'full_text')
-  String fullText;
-  int id;
-  @JsonKey(name: 'retweet_count')
-  int retweetCount;
-  @JsonKey(name: 'in_reply_to_status_id_str')
-  String inReplyToStatusIdStr;
-  bool retweeted;
+
+  /// The actual UTF-8 text of the status update.
+  ///
+  /// [text] is not included when receiving extended Tweet objects (when adding
+  /// `tweet_mode=extended` as a parameter to any endoint).
+  /// Instead [fullText] is received.
+  String text;
+
+  /// Utility used to post the Tweet, as an HTML-formatted string. Tweets from
+  /// the Twitter website have a source value of web.
   String source;
-  @JsonKey(name: 'favorite_count')
-  int favoriteCount;
-  @JsonKey(name: 'retweeted_status')
-  Tweet retweetedStatus;
-  String lang;
-  @JsonKey(name: 'display_text_range')
-  List<int> displayTextRange;
-  @JsonKey(name: 'quoted_status')
-  Tweet quotedStatus;
-  @JsonKey(name: 'quoted_status_permalink')
-  // QuotedStatusPermalink quotedStatusPermalink;
-  // @JsonKey(name: 'is_quote_status')
+
+  /// Indicates whether the value of the text parameter was truncated, for
+  /// example, as a result of a retweet exceeding the original Tweet text length
+  /// limit of 140 characters. Truncated text will end in ellipsis, like this
+  /// `...`. Since Twitter now rejects long Tweets vs truncating them, the large
+  /// majority of Tweets will have this set to `false`. Note that while native
+  /// retweets may have their toplevel text property shortened, the original
+  /// text will be available under the [retweetedStatus] object and the
+  /// truncated parameter will be set to the value of the original status (in
+  /// most cases, `false`).
+  ///
+  /// [truncated] will always be `false` when receiving extended Tweet objects.
+  bool truncated;
+
+  /// Nullable. If the represented Tweet is a reply, this field will contain the
+  /// string representation of the original Tweet’s ID.
+  String inReplyToStatusIdStr;
+
+  /// Nullable. If the represented Tweet is a reply, this field will contain the
+  /// integer representation of the original Tweet’s author ID. This will not
+  /// necessarily always be the user directly mentioned in the Tweet.
+  String inReplyToUserIdStr;
+
+  /// Nullable. If the represented Tweet is a reply, this field will contain the
+  /// screen name of the original Tweet’s author.
+  String inReplyToScreenName;
+
+  /// The user who posted this Tweet.
+  User user;
+
+  /// Nullable. Represents the geographic location of this Tweet as reported by
+  /// the user or client application.
+  // todo
+  // Coordinates coordinates;
+
+  /// Nullable. When present, indicates that the tweet is associated (but not
+  /// necessarily originating from) a Place.
+  // todo
+  // Place place;
+
+  /// This field only surfaces when the Tweet is a quote Tweet. This is the
+  /// string representation Tweet ID of the quoted Tweet.
+  String quotedStatusIdStr;
+
+  /// Indicates whether this is a Quoted Tweet.
   bool isQuoteStatus;
 
-  bool get emptyText => displayTextRange[1] == 0;
+  /// This field only surfaces when the Tweet is a quote Tweet. This attribute
+  /// contains the Tweet object of the original Tweet that was quoted.
+  Tweet quotedStatus;
+
+  /// Users can amplify the broadcast of Tweets authored by other users by
+  /// `retweeting`. Retweets can be distinguished from typical Tweets by the
+  /// existence of a [retweetedStatus] attribute. This attribute contains a
+  /// representation of the original Tweet that was retweeted. Note that
+  /// retweets of retweets do not show representations of the intermediary
+  /// retweet, but only the original Tweet. (Users can also unretweet a retweet
+  /// they created by deleting their retweet.)
+  Tweet retweetedStatus;
+
+  /// Nullable. Indicates approximately how many times this Tweet has been
+  /// quoted by Twitter users.
+  ///
+  /// Note: This object is only available with the Premium and Enterprise tier
+  /// products.
+  int quoteCount;
+
+  /// Number of times this Tweet has been replied to.
+  ///
+  /// Note: This object is only available with the Premium and Enterprise tier
+  /// products.
+  int replyCount;
+
+  /// Number of times this Tweet has been retweeted.
+  int retweetCount;
+
+  /// Nullable. Indicates approximately how many times this Tweet has been liked
+  /// by Twitter users.
+  int favoriteCount;
+
+  /// Entities which have been parsed out of the text of the Tweet.
+  // todo
+  // Entities entities;
+
+  /// When between one and four native photos or one video or one animated GIF
+  /// are in Tweet, contains an array 'media' metadata. This is also available
+  /// in Quote Tweets.
+  // todo
+  // Entities extendedEntities;
+
+  /// Nullable. Indicates whether this Tweet has been liked by the
+  /// authenticating user.
+  bool favorited;
+
+  /// Indicates whether this Tweet has been Retweeted by the authenticating
+  /// user.
+  bool retweeted;
+
+  /// Nullable. This field only surfaces when a Tweet contains a link. The
+  /// meaning of the field doesn’t pertain to the Tweet content itself, but
+  /// instead it is an indicator that the URL contained in the Tweet may contain
+  /// content or media identified as sensitive content.
+  bool possiblySensitive;
+
+  /// Nullable. When present, indicates a BCP 47 language identifier
+  /// corresponding to the machine-detected language of the Tweet text, or und
+  /// if no language could be detected.
+  String lang;
+
+  /// URL metadata for the quoted Tweet.
+  // QuotedStatusPermalink quotedStatusPermalink;
+
+  // Extended mode fields:
+
+  /// The actual UTF-8 text of the status update.
+  ///
+  /// [fullText] is only included when receiving extended Tweet objects (when
+  /// adding `tweet_mode=extended` as a parameter to any endoint).
+  ///
+  /// [fullText] is never truncated.
+  String fullText;
+
+  /// An array of two unicode code point indices, identifying the inclusive
+  /// start and exclusive end of the displayable content of the tweet.
+  @JsonKey(name: 'display_text_range')
+  List<int> displayTextRange;
 
   Map<String, dynamic> toJson() => _$TweetToJson(this);
 }
--- lib/api/tweets/data/tweet.g.dart
+++ lib/api/tweets/data/tweet.g.dart
@@ -8,44 +8,58 @@ part of 'tweet.dart';
 
 Tweet _$TweetFromJson(Map<String, dynamic> json) {
   return Tweet()
-    ..truncated = json['truncated'] as bool
-    ..favorited = json['favorited'] as bool
     ..idStr = json['id_str'] as String
-    ..inReplyToUserIdStr = json['in_reply_to_user_id_str'] as String
-    ..fullText = json['full_text'] as String
-    ..id = json['id'] as int
-    ..retweetCount = json['retweet_count'] as int
-    ..inReplyToStatusIdStr = json['in_reply_to_status_id_str'] as String
-    ..retweeted = json['retweeted'] as bool
+    ..text = json['text'] as String
     ..source = json['source'] as String
-    ..favoriteCount = json['favorite_count'] as int
-    ..retweetedStatus = json['retweeted_status'] == null
+    ..truncated = json['truncated'] as bool
+    ..inReplyToStatusIdStr = json['in_reply_to_status_id_str'] as String
+    ..inReplyToUserIdStr = json['in_reply_to_user_id_str'] as String
+    ..inReplyToScreenName = json['in_reply_to_screen_name'] as String
+    ..user = json['user'] == null
         ? null
-        : Tweet.fromJson(json['retweeted_status'] as Map<String, dynamic>)
-    ..lang = json['lang'] as String
-    ..displayTextRange =
-        (json['display_text_range'] as List)?.map((e) => e as int)?.toList()
+        : User.fromJson(json['user'] as Map<String, dynamic>)
+    ..quotedStatusIdStr = json['quoted_status_id_str'] as String
+    ..isQuoteStatus = json['is_quote_status'] as bool
     ..quotedStatus = json['quoted_status'] == null
         ? null
         : Tweet.fromJson(json['quoted_status'] as Map<String, dynamic>)
-    ..isQuoteStatus = json['quoted_status_permalink'] as bool;
+    ..retweetedStatus = json['retweeted_status'] == null
+        ? null
+        : Tweet.fromJson(json['retweeted_status'] as Map<String, dynamic>)
+    ..quoteCount = json['quote_count'] as int
+    ..replyCount = json['reply_count'] as int
+    ..retweetCount = json['retweet_count'] as int
+    ..favoriteCount = json['favorite_count'] as int
+    ..favorited = json['favorited'] as bool
+    ..retweeted = json['retweeted'] as bool
+    ..possiblySensitive = json['possibly_sensitive'] as bool
+    ..lang = json['lang'] as String
+    ..fullText = json['full_text'] as String
+    ..displayTextRange =
+        (json['display_text_range'] as List)?.map((e) => e as int)?.toList();
 }
 
 Map<String, dynamic> _$TweetToJson(Tweet instance) => <String, dynamic>{
-      'truncated': instance.truncated,
-      'favorited': instance.favorited,
       'id_str': instance.idStr,
-      'in_reply_to_user_id_str': instance.inReplyToUserIdStr,
-      'full_text': instance.fullText,
-      'id': instance.id,
-      'retweet_count': instance.retweetCount,
-      'in_reply_to_status_id_str': instance.inReplyToStatusIdStr,
-      'retweeted': instance.retweeted,
+      'text': instance.text,
       'source': instance.source,
+      'truncated': instance.truncated,
+      'in_reply_to_status_id_str': instance.inReplyToStatusIdStr,
+      'in_reply_to_user_id_str': instance.inReplyToUserIdStr,
+      'in_reply_to_screen_name': instance.inReplyToScreenName,
+      'user': instance.user?.toJson(),
+      'quoted_status_id_str': instance.quotedStatusIdStr,
+      'is_quote_status': instance.isQuoteStatus,
+      'quoted_status': instance.quotedStatus?.toJson(),
+      'retweeted_status': instance.retweetedStatus?.toJson(),
+      'quote_count': instance.quoteCount,
+      'reply_count': instance.replyCount,
+      'retweet_count': instance.retweetCount,
       'favorite_count': instance.favoriteCount,
-      'retweeted_status': instance.retweetedStatus,
+      'favorited': instance.favorited,
+      'retweeted': instance.retweeted,
+      'possibly_sensitive': instance.possiblySensitive,
       'lang': instance.lang,
+      'full_text': instance.fullText,
       'display_text_range': instance.displayTextRange,
-      'quoted_status': instance.quotedStatus,
-      'quoted_status_permalink': instance.isQuoteStatus,
     };
