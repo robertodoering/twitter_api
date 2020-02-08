a6dd06fc8bdc73a88f98f63926c3ec0b168b19ad
Roberto Doering <rbydoering@gmail.com>
2020-02-08 19:38:15 +0100

    Add url

---
+++ lib/api/common/data/url.dart
@@ -0,0 +1,36 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'url.g.dart';
+
+@JsonSerializable(
+  explicitToJson: true,
+  fieldRename: FieldRename.snake,
+)
+class Url {
+  Url();
+
+  factory Url.fromJson(Map<String, dynamic> json) => _$UrlFromJson(json);
+
+  /// URL pasted/typed into Tweet.
+  ///
+  /// Example: 'bit.ly/2so49n2'
+  String displayUrl;
+
+  /// Expanded version of [display_url].
+  ///
+  /// Example: 'http://bit.ly/2so49n2'
+  String expandedUrl;
+
+  /// An array of integers representing offsets within the Tweet text where the
+  /// URL begins and ends. The first integer represents the location of the
+  /// first character of the URL in the Tweet text. The second integer
+  /// represents the location of the first non-URL character after the end of
+  /// the URL.
+  List<int> indices;
+
+  /// Wrapped URL, corresponding to the value embedded directly into the raw
+  /// Tweet text, and the values for the indices parameter.
+  String url;
+
+  Map<String, dynamic> toJson() => _$UrlToJson(this);
+}
---
+++ lib/api/common/data/url.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'url.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Url _$UrlFromJson(Map<String, dynamic> json) {
+  return Url()
+    ..displayUrl = json['display_url'] as String
+    ..expandedUrl = json['expanded_url'] as String
+    ..indices = (json['indices'] as List)?.map((e) => e as int)?.toList()
+    ..url = json['url'] as String;
+}
+
+Map<String, dynamic> _$UrlToJson(Url instance) => <String, dynamic>{
+      'display_url': instance.displayUrl,
+      'expanded_url': instance.expandedUrl,
+      'indices': instance.indices,
+      'url': instance.url,
+    };
