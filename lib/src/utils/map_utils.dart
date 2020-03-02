e50ee8322ad868bc92b6a31f749f487ee0a333b5
Roberto Doering <rbydoering@gmail.com>
2020-03-02 22:24:51 +0100

    Add map extension to add parameters

---
+++ lib/src/utils/map_utils.dart
@@ -0,0 +1,14 @@
+extension ParameterExtension on Map<String, String> {
+  /// Adds an entry to the map.
+  ///
+  /// If [value] is `null`, it is not added to the map.
+  /// If [value] is a [List], the entry is the list joined with a `,`.
+  /// Else, the [value.toString()] is used.
+  void addParameter(String param, dynamic value) {
+    if (value is List) {
+      this[param] = value.join(',');
+    } else if (value != null) {
+      this[param] = '$value';
+    }
+  }
+}
