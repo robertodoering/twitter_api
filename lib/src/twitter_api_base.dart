3323e30493605603d46762fa4869c3970a7a8f44
Roberto Doering <rbydoering@gmail.com>
2020-04-28 16:17:32 +0200

    Add services to the twitter api base class

--- lib/src/twitter_api_base.dart
+++ lib/src/twitter_api_base.dart
@@ -1,13 +1,29 @@
 import 'package:meta/meta.dart';
+import 'package:twitter_api/api/direct_messages/direct_messages_service.dart';
+import 'package:twitter_api/api/media/media_service.dart';
+import 'package:twitter_api/api/tweets/timeline_service.dart';
+import 'package:twitter_api/api/tweets/tweet_search_service.dart';
 import 'package:twitter_api/api/tweets/tweet_service.dart';
 import 'package:twitter_api/api/twitter_client.dart';
 
 class TwitterApi {
   TwitterApi({
     @required this.client,
-  }) : tweetService = TweetService(client: client);
+  })  : tweetService = TweetService(client: client),
+        tweetSearchService = TweetSearchService(client: client),
+        timelineService = TimelineService(client: client),
+        mediaService = MediaService(client: client),
+        directMessagesService = DirectMessagesService(client: client);
 
   final TwitterClient client;
 
   final TweetService tweetService;
+
+  final TweetSearchService tweetSearchService;
+
+  final TimelineService timelineService;
+
+  final MediaService mediaService;
+
+  final DirectMessagesService directMessagesService;
 }
