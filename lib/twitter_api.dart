e7a95fe4b8bd3bc61674cee896bdc676514e8d78
Roberto Doering <rbydoering@gmail.com>
2020-06-01 00:24:04 +0200

    Add exports to twitter_api.dart

--- lib/twitter_api.dart
+++ lib/twitter_api.dart
@@ -1,7 +1,31 @@
-/// Support for doing something awesome.
+/// The [TwitterApi] provides interfaces for requests to the Twitter API and
+/// parses the returned data models.
 ///
-/// More dartdocs go here.
+/// Authentication is done via the [TwitterClient].
 library twitter_api;
 
+export 'api/abstract_twitter_client.dart';
+export 'api/common/data/entities.dart';
+export 'api/common/data/hashtag.dart';
+export 'api/common/data/option.dart';
+export 'api/common/data/poll.dart';
+export 'api/common/data/symbol.dart';
+export 'api/common/data/url.dart';
+export 'api/common/data/user_mention.dart';
+export 'api/geo/data/bounding_box.dart';
+export 'api/geo/data/coordinates.dart';
+export 'api/geo/data/place.dart';
+export 'api/media/data/media.dart';
+export 'api/media/data/media_upload.dart';
+export 'api/media/data/size.dart';
+export 'api/media/data/sizes.dart';
+export 'api/tweets/data/quoted_status_permalink.dart';
+export 'api/tweets/data/tweet.dart';
+export 'api/tweets/data/tweet_search.dart';
 export 'api/twitter_client.dart';
+export 'api/users/data/derived.dart';
+export 'api/users/data/location.dart';
+export 'api/users/data/paginated_users.dart';
+export 'api/users/data/user.dart';
 export 'src/twitter_api_base.dart';
+export 'src/utils/transforms.dart';
