00c4f6b22e9f64befad3f45644754aedc6849214
Roberto Doering <rbydoering@gmail.com>
2020-11-19 11:18:31 +0100

    Add additional trend location data

--- lib/api/trends/data/trend_location.dart
+++ lib/api/trends/data/trend_location.dart
@@ -2,9 +2,13 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'trend_location.g.dart';
 
+/// The `trends/place` response only contains [name] and [woeid] in its
+/// [TrendLocation]s.
+///
+/// For whatever reason the response keys are not in snake_case, and the id in
+/// parentid is not capitalized.
 @JsonSerializable(
   explicitToJson: true,
-  fieldRename: FieldRename.snake,
 )
 class TrendLocation {
   TrendLocation();
@@ -15,5 +19,27 @@ class TrendLocation {
   String name;
   int woeid;
 
+  String country;
+  String countryCode;
+  int parentid;
+  String url;
+  PlaceType placeType;
+
   Map<String, dynamic> toJson() => _$TrendLocationToJson(this);
+}
+
+@JsonSerializable(
+  explicitToJson: true,
+  fieldRename: FieldRename.snake,
+)
+class PlaceType {
+  PlaceType();
+
+  factory PlaceType.fromJson(Map<String, dynamic> json) =>
+      _$PlaceTypeFromJson(json);
+
+  int code;
+  String name;
+
+  Map<String, dynamic> toJson() => _$PlaceTypeToJson(this);
 }
