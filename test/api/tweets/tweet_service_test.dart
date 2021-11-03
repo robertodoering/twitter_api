aed895a384a193c6623d889df89a61190810bf5b
Demondre Livingston <livingstondemondre7@gmail.com>
2021-11-03 18:24:56 -0400

    Adding a quick fix to the retweets function

--- test/api/tweets/tweet_service_test.dart
+++ test/api/tweets/tweet_service_test.dart
@@ -133,7 +133,7 @@ void main() {
       Uri.https(
         'api.twitter.com',
         '1.1/statuses/retweets/912456.json',
-        {'tweet_mode': 'extended'},
+        {'count': '100', 'tweet_mode': 'extended'},
       ),
     )).thenAnswer(
       (_) async => Response(
